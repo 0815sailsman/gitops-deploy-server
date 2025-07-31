9e18f59752d7d3f56d1055fc5e1371f4894eda81
say <git@say.software>
2025-07-31 14:52:35 +0200

    unpack java in entrypoint.sh

--- misc/entrypoint.sh
+++ misc/entrypoint.sh
@@ -1,2 +1,12 @@
 #!/bin/sh
+
+echo "Unpacking JRE"
+zstd --decompress /opt/jre-minimal.tar.zst
+tar -xf /opt/jre-minimal.tar -C /opt
+rm /opt/jre-minimal.tar /opt/jre-minimal.tar.zst
+
+export JAVA_HOME=/opt/jre-minimal
+export PATH=$JAVA_HOME/bin:$PATH
+
+echo "Starting webhook service..."
 exec java -jar /app/app.jar
