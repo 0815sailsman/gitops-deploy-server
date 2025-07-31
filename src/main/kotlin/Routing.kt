d752b8ea4846fcf45db6b52ece92457e3d0244dd
say <git@say.software>
2025-07-31 12:40:24 +0200

    run shell command on POST and respond with result

--- src/main/kotlin/Routing.kt
+++ src/main/kotlin/Routing.kt
@@ -1,15 +1,20 @@
 package software.say
 
-import io.ktor.http.HttpMethod
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import java.io.File
 
 fun Application.configureRouting() {
     routing {
-        route("/deploy", HttpMethod.Post) {
-            handle {
-                call.respondText("Hello")
+        route("/deploy") {
+            post {
+                val deployCommand = environment.config.property("gitops.deploy_command").getString()
+                val workingDir = File(environment.config.property("gitops.environment_repo_directory").getString())
+
+                val result = deployCommand.runCommand(workingDir)
+
+                call.respondText(result ?: "Result was null!")
             }
         }
     }
